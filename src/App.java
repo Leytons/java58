9d880255372b94bf7dae474ccafecb874c3bb013
Leytons <fredleyton@outlook.com>
2021-07-01 21:13:33 -0500

    second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,5 +1,59 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo; 
+    private double estatura; 
+    private float peso;
+    private boolean estadocivil;
+
+
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //tipos de datos y operadores
+        String res = "Hola ";
+        int resultado = 15 + 32; 
+        int resultado3= 125 - 32; 
+        double resultado2 = 25 / 3; 
+        float resultado4 = 58 * 21; 
+        int resultado5 = 21 % 9; 
+        //operadores unnarios 
+        resultado++; // permite sumar 1 al resultado una adicipon de numero 1
+        resultado--; // permite restar 
+        resultado = -resultado;
+        resultado = +resultado;
+               
+        //operadores logicos. 
+        boolean respuesta = true;
+        respuesta = 5 > 8; 
+        respuesta = 5 < 8; 
+        respuesta = 5 >= 8;
+        respuesta = 5 <= 8;
+        respuesta = 12 < 5 && 4 > 1; 
+        respuesta = 14 < 25 || 1 > 5; // PARA EL FOR 
+        // ESTRUCTURAS DE CONTROL 
+        if (respuesta) {
+            
+        } else {
+            
+        } 
+        if (respuesta) {
+            
+        }
+        if (respuesta) {
+            
+        }
+
+
+
+
+
+        
+
+
+
+
     }
 }
